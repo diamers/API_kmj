7289ddfaf6731e0022b6f162ca998827284e4b4f
diamers <diamersherdi@gmail.com>
2025-11-24 17:42:05 +0700

    login commit adis

--- admin/login.php
+++ admin/login.php
@@ -1,5 +1,15 @@
 <?php
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Credentials: true");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 require __DIR__ . "/../shared/config.php";
 require __DIR__ . "/../shared/jwt_helper.php";
 
